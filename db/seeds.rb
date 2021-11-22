aff068e4b14835468879eb53990eb10015c881da
Ryan Brouwer <mennofromheavan@gmail.com>
2021-11-22 17:38:18 +0000

    we have seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "adding games to db"
+
+categories = ['strategy', 'cards', 'dice', 'puzzles' ]
+
+20.times do
+  Game.create!(
+    title: Faker::Game.title,
+    category: categories.select,
+    difficulty: rand(1..5),
+    player_count: rand(2..10),
+    play_time: rand(0.5..4.0),
+    price: rand(5..20),
+    user_id: rand(1..3)
+  )
+
+
+end
+
+puts "games added"
