ea4c7c6ef765ed0b6e057243074f20ede6030526
Flynn Lebourgeois <flynnn@me.com>
2021-11-26 12:41:39 +0000

    cards done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,8 +28,3 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
 });
-
-var someTabTriggerEl = document.querySelector('#someTabTrigger')
-var tab = new bootstrap.Tab(someTabTriggerEl)
-
-tab.show()
